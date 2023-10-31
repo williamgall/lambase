e66e5cd34632733be58102b7cc8948f2b1b15737
William Gall <gallwill@msu.edu>
2023-10-30 21:39:13 -0400

    Changed Base to BaseModel and updated to latest

--- src/Model/BaseModel.php
+++ src/Model/BaseModel.php
@@ -4,17 +4,16 @@ namespace williamgall\lambase\Model;
 
 use Laminas\Config\Config;
 use Laminas\Db\Adapter\Adapter;
+use Laminas\Db\Adapter\Driver\StatementInterface;
 use Laminas\Db\Adapter\ParameterContainer;
+use Laminas\Db\Adapter\Driver\ResultInterface;
 use Laminas\Db\ResultSet\ResultSet;
-use Laminas\Db\Adapter\Profiler\Profiler;
+use Laminas\Db\ResultSet\ResultSetInterface;
 use Laminas\Db\Sql\Delete;
 use Laminas\Db\Sql\Select;
 use Laminas\Db\Sql\Sql;
-use Laminas\Mail\Message;
-use Laminas\Mail\Transport\Smtp as SmtpTransport;
-use Laminas\Mail\Transport\SmtpOptions;
 
-class Base
+class BaseModel
 {
     const RESULTS_TO_ARRAY = 1;
 
@@ -29,19 +28,26 @@ class Base
      * getConfig
      * gets the applications configuration
      * @return array
+     * Tested
      */
     public function getConfig()
     {
-        // always use local.php if it exists 
+        // always use local.php if it exists
         if(!file_exists(__DIR__ . '/../../../../config/autoload/local.php'))
         {
             return include __DIR__ . '/../../../../config/autoload/global.php';
         }
         return include __DIR__ . '/../../../../config/autoload/local.php';
     }
+
+    /**
+     *
+     * Tested
+     * @return mixed
+     */
     public function getLocalConfig()
     {
-        return include __DIR__ . '/../../../../config/autoload/local.php';
+        return include './config/autoload/local.php';
     }
     public function getMergedConfig()
     {
@@ -49,10 +55,14 @@ class Base
         $local = $this->getLocalConfig();
         return array_merge($global,$local);
     }
-    
+
+    /**
+     * Tested
+     * @return mixed
+     */
     public function getGlobConfig()
     {
-        return include __DIR__ . '/../../../../config/autoload/global.php';
+        return include './config/autoload/global.php';
     }
 
     public function __construct($params = [])
@@ -63,41 +73,24 @@ class Base
         {
             if(empty($this->adapter))
             {
-                $this->profiler = new Profiler();
                 $this->adapter = new Adapter(
-                    $config['dbs']['adapters'][$this->useAdapter],
-                    NULL,
-                    NULL,
-                    $this->profiler
+                    $config['dbs']['adapters'][$this->useAdapter]
                 );
             }
         }
         if (! isset($config['dbs']['adapters'][$this->useAdapter]))
         {
-            throw new \Exception('No Config for '.$this->useAdapter." ".__FILE__); 
+            throw new \Exception('No Config for '.$this->useAdapter." ".__FILE__);
         }
         $this->dbDriver = $config['dbs']['adapters'][$this->useAdapter]['driver'];
         $this->sql = new Sql($this->adapter);
         $this->setParams($params);
     }
-    
-    public function getProfile()
-    {
-        $profiler = $this->adapter->getProfiler();
-        return $profiler->getLastProfile();
-    }
-    
-    public function getProfiles()
-    {
-        $profiler = $this->adapter->getProfiler();
-        return $profiler->getProfiles();
-    }
-    
+
+    // TODO Probably not the best spot for this
     public function devSendMail()
     {
         $config = $this->getConfig();
-//         print_r($config['devOptions']['sendmail'],0);
-//         exit;
         if (isset($config['devOptions']) && isset($config['devOptions']['sendmail']))
         {
             if ($config['devOptions']['sendmail'] == 0 || $config['devOptions']['sendmail'] == "FALSE" )
@@ -110,6 +103,7 @@ class Base
         }
         return "Error";
     }
+
     /**
      * getUseAdapter
      *
@@ -121,12 +115,18 @@ class Base
     {
         return $this->useAdapter;
     }
-    
+
     public function setParams($params)
     {
         $this->params = $params;
     }
 
+    /**
+     * Tested
+     * @param $query
+     * @param $wheres
+     * @return void
+     */
     public function setWhere(&$query, $wheres)
     {
         if(is_numeric($wheres))
@@ -147,7 +147,13 @@ class Base
             }
         }
     }
-    
+
+    /**
+     *
+     * Tested
+     * @param $formData
+     * @return array
+     */
     public function convertFormData($formData)
     {
         $dataArray = array();
@@ -188,10 +194,18 @@ class Base
     }
     public function query2($sql, $vars = [])
     {
+        // TODO Test
         $results = $this->adapter->query($sql, new ParameterContainer($vars));
 //         return $results->toArray();
     }
-    
+
+    /**
+     *
+     * Tested
+     * @param $sql
+     * @param $vars
+     * @return array|mixed
+     */
     public function fetchRow($sql, $vars = [])
     {
         if(! is_array($vars))
@@ -206,12 +220,13 @@ class Base
         }
         return $resultsArray;
     }
-    
+
     /**
      * fetchAll
-     * 
+     *
      * fetches all rows from query
-     * 
+     *
+     * Tested
      * @param string $sql
      * @param array $vars
      * @return array
@@ -225,7 +240,7 @@ class Base
         $results = $this->adapter->query($sql, new ParameterContainer($vars));
         return $results->toArray();
     }
-    
+
     public function getSelectObject($table = '')
     {
         if(empty($table))
@@ -278,6 +293,11 @@ class Base
         return $results->toArray();
     }
 
+    /**
+     * Tested
+     * @param $data
+     * @return mixed
+     */
     public function insert($data)
     {
         $insert = $this->sql->insert($this->tableName);
@@ -292,20 +312,67 @@ class Base
         return $id;
     }
 
-    public function insertToTable($data,$table)
+    /**
+     * Tested
+     * @param $data
+     * @param $table
+     * @return mixed
+     */
+    public function insertToTable($data, $table)
     {
         $insert = $this->sql->insert($table);
-        
+
         $insert->values($data);
         $sqlString = $this->sql->buildSqlString($insert);
-        
+
         $this->adapter->query($sqlString, Adapter::QUERY_MODE_EXECUTE);
-        
+
         $id = $this->adapter->getDriver()->getLastGeneratedValue();
-        
+
         return $id;
     }
-    
+
+//    public function beginTrans()
+//    {
+//        $this->adapter->getDriver()->getConnection()->beginTransaction();
+//    }
+
+    public function insertToTableTrans($data,$table=NULL)
+    {
+        if ($table===NULL)
+        {
+            $insert = $this->sql->insert($this->tableName);
+        }else{
+            $insert = $this->sql->insert($table);
+        }
+
+        $insert->values($data);
+        $statement1 = $this->sql->prepareStatementForSqlObject($insert);
+        $result1 = $statement1->execute();
+        if ($result1 instanceof ResultInterface && $result1->getAffectedRows())
+        {
+            return 1;
+
+        }else{
+            return 0;
+        }
+    }
+//    public function commitTrans()
+//    {
+//        $this->adapter->getDriver()-getConnection()->commit();
+//    }
+
+//    public function rollbackTrans()
+//    {
+//        $this->adapter->getDriver()->getConnection()->rollback();
+//    }
+    /**
+     *
+     * Tested
+     * @param array $data
+     * @param int || array $where
+     *
+     */
     public function update($data, $where)
     {
         if(empty($where))
@@ -329,52 +396,77 @@ class Base
         $results = $this->adapter->query($sqlString, Adapter::QUERY_MODE_EXECUTE);
         return $results;
     }
-    
+
+    /**
+     *
+     * Tested
+     * @param $data
+     * @param $where
+     * @param $table
+     * @return false|StatementInterface|ResultSet|ResultSetInterface
+     */
     public function updateTable($data, $where, $table)
     {
         if(empty($where) || empty($table))
         {
             return false;
         }
-        
+
         $update = $this->sql->update($table);
         $update->set($data);
-        
+
         if(is_numeric($where))
         {
-            
+
             $where = [$this->pkId => $where];
         }
         $this->setWhere($update, $where);
-        
+
         $sqlString = $this->sql->buildSqlString($update);
-        
+
         $results = $this->adapter->query($sqlString, Adapter::QUERY_MODE_EXECUTE);
         return $results;
     }
+
+    /**
+     * delete
+     *
+     * deletes a row from the table by id
+     * Tested
+     *
+     * @param int $id
+     */
     public function delete($id)
     {
         $delete = $this->getDeleteObject();
         $delete->where([$this->pkId => $id]);
         $this->runQueryBuilt($delete);
     }
-    
+
+    /**
+     * Tested
+     * @param $where
+     * @return false|StatementInterface|ResultSet|ResultSetInterface
+     */
     public function deleteWhere($where)
     {
         if(empty($where))
         {
-            
             return false;
         }
-        
+
         $delete = $this->sql->delete($this->tableName);
-        
-        
+
         $this->setWhere($delete, $where);
-        
+
         $sqlString = $this->sql->buildSqlString($delete);
-//         error_log($sqlString);
         return $this->adapter->query($sqlString, Adapter::QUERY_MODE_EXECUTE);
+    }
+    public function deleteIdFromTable($id, $tableName)
+    {
+        $delete = $this->sql->delete($tableName);
+        $delete->where([$this->pkId => $id]);
+        return $this->runQueryBuilt($delete);
     }
 
     public function setLimit(&$select)
@@ -406,7 +498,14 @@ class Base
             $this->adapter->getDriver()->getConnection()->disconnect();
         }
     }
-    
+
+    /**
+     *
+     * Tested
+     * @param $data
+     * @param $pretty
+     * @return void
+     */
     public function debug($data, $pretty=0)
     {
         if ($_SERVER['APPLICATION_ENV'] == 'development' || $_SERVER['APPLICATION_ENV'] == 'testing')
@@ -419,7 +518,7 @@ class Base
                 {
                     var_export($data);
                 }else{
-                print_r($data,FALSE);
+                    print_r($data,FALSE);
                 }
                 echo "</pre>";
                 exit;
@@ -446,130 +545,70 @@ class Base
             }
         }
     }
-    
+
+    /**
+     * Tested
+     * @param $data
+     * @return void
+     */
     public function exportData($data)
     {
         echo "// Array with: ".count($data)." Rows";
 
-            var_export($data);
-       
+        var_export($data);
+
         exit;
     }
-    public function mailAdmin($messageText)
-    {
-        if ($this->devSendMail() == FALSE && $_SERVER['APPLICATION_ENV'] == 'development')
-        {
-//             error_log('skipping mail in dev');
-            return;
-        }
-        $admin = DEV_EMAIL;
-        $message = new Message();
-        $subject = $messageText;
-        
-        
-        $body = $messageText;
-        
-        $message->addTo($admin);
-        
-        // Add environment to subject
-        $subject = "[EASP-".APPLICATION_ENV."] ".$subject;
-        
-        
-        $message->addFrom('abroad@msu.edu','Education Abroad');
-        
-        
-        $message->addTo($admin);
-        
-        $message->setSubject($subject);
-        $message->setBody($body);
-        $message->setEncoding('UTF-8');
-        
-        $transport = new SmtpTransport();
-        $config = $this->getConfig();
-        $options   = new SmtpOptions($config['smtpOptions']);
-        $transport->setOptions($options);
-        $transport->send($message);
-    }
-    
+
     /**
-     * sendEmail
-     * 
-     * @param string | array $messageTo
-     * @param string $subject
-     * @param string $messageText
+     * Tested
+     * @param $array1
+     * @param $array2
+     * @return array
      */
-    public function sendEmail($messageTo, $subject, $messageText)
-    {
-        if ($this->devSendMail() == 0 && $_SERVER['APPLICATION_ENV'] == 'development')
-        {
-            // Return without sending mail
-            return;
-        }
-        
-        $message = new Message();
-        
-        
-        $body = $messageText;
-        if (APPLICATION_ENV == 'production')
-        {
-            if (is_array($messageTo))
-            {
-                foreach ($messageTo as $recip)
-                {
-                    $message->addTo($recip);
-                }
-            }else{
-                $message->addTo($messageTo);
-            }
-            $message->addCc(DEV_EMAIL);
-        }else{
-            $message->addTo(DEV_EMAIL);
-            $header = "On production the email would have gone to the following recipients: ";
-            if (is_array($messageTo))
-            {
-                foreach ($messageTo as $recip)
-                {
-                    $header .= $recip.",";
-                }
-            }else{
-                $header .= $messageTo;
-            }
-        }
-        
-        // Add environment to subject
-        $subject = "[EASP-".APPLICATION_ENV."] ".$subject;
-        
-        $message->addFrom('abroad@msu.edu','Education Abroad');
-        
-        $message->setSubject($subject);
-        
-        if ($header <> "")
-        {
-            $body = $header."\n\n".$body;
-        }
-        $message->setBody($body);
-        $message->setEncoding('UTF-8');
-        
-        $transport = new SmtpTransport();
-        $config = $this->getConfig();
-        $options   = new SmtpOptions($config['smtpOptions']);
-        $transport->setOptions($options);
-        $transport->send($message);
-    }
-    
     public function compareArray($array1, $array2)
     {
         return array_diff_assoc($array1, $array2);
     }
-    
+
     public function getGlobalConfig()
     {
         $path = __DIR__."/../../../..";
-        
+
         $config = new Config(include($path."/config/autoload/global.php"),TRUE);
         $configlocal = new Config(include( $path."/config/autoload/local.php"));
         $config->merge($configlocal);
         $config->setReadOnly();
         return $config;
+    }
+
+    /**
+     *
+     * Tested
+     * @param $tableName
+     * @param $dbName
+     * @return mixed
+     */
+    public function getNextAutoIncValue($tableName, $dbName)
+    {
+        $result = $this->fetchRow("SELECT AUTO_INCREMENT FROM information_schema.tables WHERE table_name = '".$tableName."' AND table_schema = '".$dbName."'");
+        return $result['AUTO_INCREMENT'];
+    }
+
+    /**
+     *
+     * Tested
+     * @param string $table
+     * @return array
+     */
+    public function getColumnsForTable(string $table)
+    {
+        $returnArray = [];
+        $result = $this->fetchAll("SELECT column_name FROM information_schema.columns WHERE table_name = ?",$table);
+        foreach ($result as $item)
+        {
+            $returnArray[] = $item['column_name'];
+        }
+        return $returnArray;
     }
 }
